df3fa0284458d7d1fd44460902eb8de186cf8741
Anuj Verma <vanuj010@gmail.com>
2026-02-10 22:08:08 +0530

    DB file change acc. to rilway

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
+import os
 import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="9582",
-        database="retech"
+        host=os.environ.get("mysql.railway.internal"),
+        user=os.environ.get("root"),
+        password=os.environ.get("KzrPQbswqyGVXGGVuTumDKCQidBSRyFd"),
+        database=os.environ.get("railway")
     )
-
