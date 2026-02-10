93e0d3d8477617a5013e4c25813f2efcec5b612c
Anuj Verma <vanuj010@gmail.com>
2026-02-10 22:44:00 +0530

    blank

--- db.py
+++ db.py
@@ -2,6 +2,10 @@ import os
 import mysql.connector
 
 def get_db():
+    print("DB_HOST =", os.environ.get("DB_HOST"))
+    print("DB_USER =", os.environ.get("DB_USER"))
+    print("DB_NAME =", os.environ.get("DB_NAME"))
+
     return mysql.connector.connect(
         host=os.environ.get("DB_HOST"),
         user=os.environ.get("DB_USER"),
