0b9e4ac7fcb3beb9f62a59c9a005fc413b7444b1
Anuj Verma <vanuj010@gmail.com>
2026-02-13 21:16:07 +0530

    DB.PY chnage

--- db.py
+++ db.py
@@ -1,15 +1,14 @@
 import os
 import mysql.connector
+from urllib.parse import urlparse
 
 def get_db():
-    print("DB_HOST =", os.environ.get("DB_HOST"))
-    print("DB_USER =", os.environ.get("DB_USER"))
-    print("DB_NAME =", os.environ.get("DB_NAME"))
-
+    url = urlparse(os.environ["DATABASE_URL"])
     return mysql.connector.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME"),
-        port=3306
+        host=url.hostname,
+        user=url.username,
+        password=url.password,
+        database=url.path.lstrip("/"),
+        port=url.port,
+        ssl_disabled=False
     )
